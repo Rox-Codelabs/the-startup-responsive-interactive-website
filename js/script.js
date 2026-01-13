70b2f06159f226dd4247f037a75f2d26f9f42753
Roxy <149881577+Rox-Codelabs@users.noreply.github.com>
2026-01-13 02:21:52 +0100

    @8 hamburger menu toggle met svg

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
 //--------------------------------------------------------------------------------------------------------------------------------------------------
+// --------------------------------- hoofdnavigatie open
+// Stap 1
+const menu = document.querySelector('.hoofdnavigatie');
+const menuBtn = document.querySelector('.hoofdnavigatie-btn');
+const svgOpen = menuBtn.querySelector('.menu-open-btn');
+const svgClose = menuBtn.querySelector('.menu-close-btn');
+// Stap 2
+menuBtn.addEventListener('click', function(){
+    // Stap 3
+    menu.classList.toggle('hidden');
+    svgOpen.classList.toggle('hidden');      
+    svgClose.classList.toggle('hidden'); 
+});
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------
 // ---------------------------------diensten submenu open en dicht
 // Stap 1
 const caretD = document.querySelector('.caret-diensten');
