67df4b065be2b35c29c8a1735d78b3d0a60f5db1
Roxy <149881577+Rox-Codelabs@users.noreply.github.com>
2026-01-14 01:41:37 +0100

    #8 javascript 3 stappenplan

--- js/script.js
+++ js/script.js
@@ -3,33 +3,37 @@
 // Stap 1
 const menu = document.querySelector('.hoofdnavigatie');
 const menuBtn = document.querySelector('.hoofdnavigatie-btn');
-const svgOpen = menuBtn.querySelector('.menu-open-btn');
-const svgClose = menuBtn.querySelector('.menu-close-btn');
+const svgOpen = document.querySelector('.menu-open-btn');
+const svgClose = document.querySelector('.menu-close-btn');
+const logo = document.querySelector('.logo');
 // Stap 2
 menuBtn.addEventListener('click', function(){
     // Stap 3
     menu.classList.toggle('hidden');
     svgOpen.classList.toggle('hidden');      
     svgClose.classList.toggle('hidden'); 
+    logo.classList.toggle('hidden');
+    document.body.classList.toggle('menu-open');
 });
 
 //--------------------------------------------------------------------------------------------------------------------------------------------------
 // ---------------------------------diensten submenu open en dicht
 // Stap 1
-const caretD = document.querySelector('.caret-diensten');
+const caretD = document.querySelector('.diensten .menu-caret');
 const submenuDiensten = document.getElementById('diensten-submenu');
 // Stap 2
-caretD.addEventListener('click', function(){
+caretD.addEventListener('click', function() {
     // Stap 3
     submenuDiensten.classList.toggle('hidden');
 });
 
+//--------------------------------------------------------------------------------------------------------------------------------------------------
 // ---------------------------------overons submenu open en dicht
 // Stap 1
-const caretO = document.querySelector('.caret-overons');
+const caretO = document.querySelector('.overons .menu-caret');
 const submenuOverons = document.getElementById('overons-submenu');
 // Stap 2
-caretO.addEventListener('click', function(){
+caretO.addEventListener('click', function() {   // <-- hier moet caretO staan
     // Stap 3
     submenuOverons.classList.toggle('hidden');
 });
